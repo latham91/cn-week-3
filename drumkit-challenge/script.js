5320e8f25d96cdc8ce751bc01f5e00727f25fe14
Aaron Latham <latham91@icloud.com>
2024-01-09 14:36:15 +0000

    refactored drumkit code

--- drumkit-challenge/script.js
+++ drumkit-challenge/script.js
@@ -17,8 +17,10 @@ const snareAnim = document.querySelector(".snare-top");
 const tomAnim = document.querySelector(".tom-top");
 const snareBody = document.querySelector(".snare-body-outer");
 const tomBody = document.querySelector(".tom-body-outer");
+const keysContainer = document.querySelector(".keys-container");
 
-bassAnim.addEventListener("click", () => {
+// Refactor
+const playBase = () => {
     bassAnim.style.animation = "bass 0.2s linear";
 
     setTimeout(() => {
@@ -28,9 +30,15 @@ bassAnim.addEventListener("click", () => {
     boom.pause();
     boom.currentTime = 0;
     boom.play();
-});
+};
 
-hihatAnimBottom.addEventListener("click", () => {
+const playClap = () => {
+    clap.pause();
+    clap.currentTime = 0;
+    clap.play();
+};
+
+const playHihat = () => {
     hihatAnimTop.style.animation = "hihatTopRotate 0.2s linear";
     hihatAnimBottom.style.animation = "hihatBottomRotate 0.2s linear";
 
@@ -42,9 +50,21 @@ hihatAnimBottom.addEventListener("click", () => {
     hihat.pause();
     hihat.currentTime = 0;
     hihat.play();
-});
+};
 
-rideAnimBottom.addEventListener("click", () => {
+const playKick = () => {
+    bassAnim.style.animation = "bass 0.2s linear";
+
+    setTimeout(() => {
+        bassAnim.style.removeProperty("animation");
+    }, 200); // Removes animation after 200ms
+
+    kick.pause();
+    kick.currentTime = 0;
+    kick.play();
+};
+
+const playRide = () => {
     rideAnimTop.style.animation = "rideRotate 0.2s linear";
     rideAnimBottom.style.animation = "rideRotate 0.2s linear";
 
@@ -56,9 +76,23 @@ rideAnimBottom.addEventListener("click", () => {
     ride.pause();
     ride.currentTime = 0;
     ride.play();
-});
+};
 
-snareBody.addEventListener("click", () => {
+const playOpenHat = () => {
+    hihatAnimTop.style.animation = "hihatTopOpen 0.2s linear";
+    hihatAnimBottom.style.animation = "hihatBottomOpen 0.2s linear";
+
+    setTimeout(() => {
+        hihatAnimTop.style.removeProperty("animation");
+        hihatAnimBottom.style.removeProperty("animation");
+    }, 200); // Removes animation after 200ms
+
+    openhat.pause();
+    openhat.currentTime = 0;
+    openhat.play();
+};
+
+const playSnare = () => {
     snareAnim.style.animation = "tomShake 0.2s linear";
 
     setTimeout(() => {
@@ -68,9 +102,15 @@ snareBody.addEventListener("click", () => {
     snare.pause();
     snare.currentTime = 0;
     snare.play();
-});
+};
 
-tomBody.addEventListener("click", () => {
+const playTink = () => {
+    tink.pause();
+    tink.currentTime = 0;
+    tink.play();
+};
+
+const playTom = () => {
     tomAnim.style.animation = "tomShake 0.2s linear";
 
     setTimeout(() => {
@@ -80,92 +120,67 @@ tomBody.addEventListener("click", () => {
     tom.pause();
     tom.currentTime = 0;
     tom.play();
+};
+
+keysContainer.childNodes.forEach((key, index) => {
+    key.addEventListener("click", () => {
+        switch (index) {
+            case 1:
+                playBase();
+                break;
+            case 3:
+                playClap();
+                break;
+            case 5:
+                playHihat();
+                break;
+            case 7:
+                playKick();
+                break;
+            case 9:
+                playOpenHat();
+                break;
+            case 11:
+                playRide();
+                break;
+            case 13:
+                playSnare();
+                break;
+            case 15:
+                playTink();
+                break;
+            case 17:
+                playTom();
+                break;
+        }
+    });
 });
+
+bassAnim.addEventListener("click", playBase);
+hihatAnimBottom.addEventListener("click", playHihat);
+hihatAnimTop.addEventListener("click", playOpenHat);
+rideAnimBottom.addEventListener("click", playRide);
+snareBody.addEventListener("click", playSnare);
+tomBody.addEventListener("click", playTom);
 
 document.addEventListener("keypress", (e) => {
     if (e.keyCode === 65 || e.keyCode === 97) {
-        bassAnim.style.animation = "bass 0.2s linear";
-
-        setTimeout(() => {
-            bassAnim.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        boom.pause();
-        boom.currentTime = 0;
-        boom.play();
+        playBase();
     } else if (e.keyCode === 83 || e.keyCode === 115) {
-        clap.pause();
-        clap.currentTime = 0;
-        clap.play();
+        playClap();
     } else if (e.keyCode === 68 || e.keyCode === 100) {
-        hihatAnimTop.style.animation = "hihatTopRotate 0.2s linear";
-        hihatAnimBottom.style.animation = "hihatBottomRotate 0.2s linear";
-
-        setTimeout(() => {
-            hihatAnimTop.style.removeProperty("animation");
-            hihatAnimBottom.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        hihat.pause();
-        hihat.currentTime = 0;
-        hihat.play();
+        playHihat();
     } else if (e.keyCode === 70 || e.keyCode === 102) {
-        bassAnim.style.animation = "bass 0.2s linear";
-
-        setTimeout(() => {
-            bassAnim.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        kick.pause();
-        kick.currentTime = 0;
-        kick.play();
+        playKick();
     } else if (e.keyCode === 71 || e.keyCode === 103) {
-        hihatAnimTop.style.animation = "hihatTopOpen 0.2s linear";
-        hihatAnimBottom.style.animation = "hihatBottomOpen 0.2s linear";
-
-        setTimeout(() => {
-            hihatAnimTop.style.removeProperty("animation");
-            hihatAnimBottom.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        openhat.pause();
-        openhat.currentTime = 0;
-        openhat.play();
+        playOpenHat();
     } else if (e.keyCode === 72 || e.keyCode === 104) {
-        rideAnimTop.style.animation = "rideRotate 0.2s linear";
-        rideAnimBottom.style.animation = "rideRotate 0.2s linear";
-
-        setTimeout(() => {
-            rideAnimTop.style.removeProperty("animation");
-            rideAnimBottom.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        ride.pause();
-        ride.currentTime = 0;
-        ride.play();
+        playRide();
     } else if (e.keyCode === 74 || e.keyCode === 106) {
-        snareAnim.style.animation = "tomShake 0.2s linear";
-
-        setTimeout(() => {
-            snareAnim.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        snare.pause();
-        snare.currentTime = 0;
-        snare.play();
+        playSnare();
     } else if (e.keyCode === 75 || e.keyCode === 107) {
-        tink.pause();
-        tink.currentTime = 0;
-        tink.play();
+        playTink();
     } else if (e.keyCode === 76 || e.keyCode === 108) {
-        tomAnim.style.animation = "tomShake 0.2s linear";
-
-        setTimeout(() => {
-            tomAnim.style.removeProperty("animation");
-        }, 200); // Removes animation after 200ms
-
-        tom.pause();
-        tom.currentTime = 0;
-        tom.play();
+        playTom();
     }
 });
