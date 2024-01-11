df05cd746dd9302d4d7d2af9fbe08d54a17a0db0
Aaron Latham <latham91@icloud.com>
2024-01-11 18:19:30 +0000

    added function for snake movement

--- snake/script.js
+++ snake/script.js
@@ -33,3 +33,39 @@ const drawFood = () => {
     ctx.fillStyle = "red";
     ctx.fillRect(food.x, food.y, grid, grid);
 };
+
+const drawScore = () => {
+    ctx.fillStyle = "black";
+    ctx.font = "40px Monospace";
+    ctx.fillText(`Score: ${score}`, 32, 64);
+};
+
+const moveSnake = () => {
+    const head = {
+        x: snake[0].x,
+        y: snake[0].y,
+    };
+
+    // This is the movement of the snake
+    if (direction === "up") {
+        head.y -= grid;
+    } else if (direction === "down") {
+        head.y += grid;
+    } else if (direction === "left") {
+        head.x -= grid;
+    } else if (direction === "right") {
+        head.x += grid;
+    }
+
+    snake.unshift(head);
+
+    if (snake[0].x === food.x && snake[0].y === food.y) {
+        score++;
+        food = {
+            x: Math.floor(Math.random() * 32) * grid,
+            y: Math.floor(Math.random() * 32) * grid,
+        };
+    } else {
+        snake.pop();
+    }
+};
